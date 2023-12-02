a8ad54dfd7b08c9b90c6d08eeaa31ab9677fffb4
Bryce Cole <brycewcole@gmail.com>
2023-12-02 12:49:25 -0500

    Day 2!

--- day2/part1/solution.py
+++ day2/part1/solution.py
@@ -1,6 +1,23 @@
 def main():
     with open('day2/part1/input.txt', 'r') as f:
-        pass
+        possibleGames = []
+        target = {'red' : 12, 'blue' : 14, 'green' : 13}
+        lines = [line.strip() for line in f.readlines()]
+        for line in lines:
+            id = int(line.split()[1][:-1])
+            game = ' '.join(line.split()[2:])
+            gameValues = {'red' : 0, 'blue' : 0, 'green' : 0} 
+            draws = game.split(';')
+            for draw in draws:
+                for group in draw.split(','):
+                    count, color = group.strip().split()
+                    gameValues[color] = max(gameValues[color], int(count))
+            
+            if gameValues['red'] <= target['red'] and gameValues['blue'] <= target['blue'] and gameValues['green'] <= target['green']:
+                possibleGames.append(id)
+            
+        print(sum(possibleGames))
+        
 
 if __name__ == '__main__':
     main()
--- day2/part2/solution.py
+++ day2/part2/solution.py
@@ -1,6 +1,20 @@
 def main():
     with open('day2/part2/input.txt', 'r') as f:
-        pass
+        powers = []
+        lines = [line.strip() for line in f.readlines()]
+        for line in lines:
+            id = int(line.split()[1][:-1])
+            game = ' '.join(line.split()[2:])
+            gameValues = {'red' : 0, 'blue' : 0, 'green' : 0} 
+            draws = game.split(';')
+            for draw in draws:
+                for group in draw.split(','):
+                    count, color = group.strip().split()
+                    gameValues[color] = max(gameValues[color], int(count))
+
+            powers.append(gameValues['red'] * gameValues['blue'] * gameValues['green'])
+            
+        print(sum(powers))
 
 if __name__ == '__main__':
     main()
